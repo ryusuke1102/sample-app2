46617d542edfb76f181ab9ade4a13dc255e46519
ryu <ryusukeminee@gmail.com>
2019-10-12 19:31:34 +0900

    first static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
   end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
+
 
 end
