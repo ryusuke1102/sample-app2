06b25a048cb6773587e93d326a14f0b72569b884
ryu <ryusukeminee@gmail.com>
2019-10-13 19:24:05 +0900

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   get '/about',to:'static_pages#about'
   get '/help',to:'static_pages#help'
+  get '/contact',to: 'static_pages#contact'
 
+  get '/signup',to: 'user#new'
   
 end
