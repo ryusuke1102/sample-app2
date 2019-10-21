b4b4c88abc6555fe1aeb6ca72699e5161c6f45ed
ryu <ryusukeminee@gmail.com>
2019-10-21 21:15:51 +0900

    Implement basic login

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get login_path
+    assert_response :success
+    assert_select "title", "Log in | Ruby on Rails Tutorila Sample App"
+  end
+
+end
