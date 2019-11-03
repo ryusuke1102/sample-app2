12ad749bba92f2665902c420de9a124ede10428c
ryu <ryusukeminee@gmail.com>
2019-11-03 18:58:28 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
     validates :name,  presence: true, length: { maximum: 50 }
@@ -12,7 +12,6 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 },allow_nil: true
 
 
-     # 渡された文字列のハッシュ値を返す
     # 渡された文字列のハッシュ値を返す
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -52,6 +51,24 @@ class User < ApplicationRecord
         def send_activation_email
             UserMailer.account_activation(self).deliver_now
         end
+
+        # パスワード再設定の属性を設定する
+        def create_reset_digest
+            self.reset_token = User.new_token
+            update_attribute(:reset_digest,  User.digest(reset_token))
+            update_attribute(:reset_sent_at, Time.zone.now)
+        end
+
+        # パスワード再設定のメールを送信する
+        def send_password_reset_email
+            UserMailer.password_reset(self).deliver_now
+        end
+
+        #　パスワード再設定の期限が切れている場合tureを返す
+        def password_reset_expired?
+            reset_sent_at < 2.hours.ago
+        end
+
 
     private
 
